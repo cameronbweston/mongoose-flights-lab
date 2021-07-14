ec4d90e5cc342225eff3413f7e124da1e0ad9a88
Cameron Weston <cameronbweston@gmail.com>
2021-07-14 17:14:54 -0500

    Add event listener for db

--- config/database.js
+++ config/database.js
@@ -5,3 +5,10 @@ mongoose.connect(process.env.DATABASE_URL, {
   useUnifiedTopology: true,
   useCreateIndex: true,
 })
+
+// shortcut to mongoose.connection object
+const db = mongoose.connection
+
+db.on('connected', function() {
+  console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`)
+})
