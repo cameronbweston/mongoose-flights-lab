5a689bcf247abf6d046049843c8e6033f38a8c7f
Cameron Weston <cameronbweston@gmail.com>
2021-07-15 09:26:38 -0500

    Add enums to the Flight schema

--- models/flights.js
+++ models/flights.js
@@ -7,10 +7,30 @@ export {
 //optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema;
 
+const airlines = { 
+    'American': 1,
+    'Southwest': 2,
+    'United': 3
+}
+
+const airports = {
+    'AUS': 1,
+    'DFW': 2,
+    'DEN': 3,
+    'LAX': 4,
+    'SAN': 5
+}
+
 //Model
 const flightSchema = new Schema({
-    airline: String, //enum to include American, Southwest, and United
-    airport: String, //enum to include AUS, DFW, DEN, LAX, SAN
+    airline: {
+        type: String,
+        enum: airlines,
+    }, //enum to include American, Southwest, and United
+    airport: { 
+        type: String,
+        enum: airports,
+        default: 'DEN'}, //enum to include AUS, DFW, DEN, LAX, SAN
     flightNo: Number, //Required Between 10 and 9999
     departs: Date //n/a
 })
