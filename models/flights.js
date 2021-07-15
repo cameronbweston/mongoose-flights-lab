e7a3d32eb77800fb10790d385aeeb69393000aab
Cameron Weston <cameronbweston@gmail.com>
2021-07-15 12:43:25 -0500

    Fix form validation

--- models/flights.js
+++ models/flights.js
@@ -7,33 +7,21 @@ export {
 //optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema;
 
-const airlines = { 
-    'American': 1,
-    'Southwest': 2,
-    'United': 3
-}
-
-const airports = {
-    'AUS': 1,
-    'DFW': 2,
-    'DEN': 3,
-    'LAX': 4,
-    'SAN': 5
-}
-Object.freeze(airlines);
-Object.freeze(airports);
-
 //Model
 const flightSchema = new Schema({
     airline: {
         type: String,
-        enum: airlines,
+        enum: ['American', 'Southwest', 'United'],
     }, //enum to include American, Southwest, and United
     airport: { 
         type: String,
-        enum: airports,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
         default: 'DEN'}, //enum to include AUS, DFW, DEN, LAX, SAN
-    flightNo: Number, //Required Between 10 and 9999
+    flightNo: {
+        type: Number,
+        min: 10,
+        max: 9999,
+    }, //Required Between 10 and 9999
     departs: Date //n/a
 })
 
