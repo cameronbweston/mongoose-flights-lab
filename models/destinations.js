69f82bb23f4691177873a899c166076bff1e6b59
Cameron Weston <cameronbweston@gmail.com>
2021-07-20 19:39:48 -0500

    Remove restrictive enum from destinations so user can enter anything

--- models/destinations.js
+++ models/destinations.js
@@ -9,7 +9,6 @@ const Schema = mongoose.Schema;
 const destinationSchema = Schema({
     airport: {
         type: String,
-        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
         unique: true
     }
 })
